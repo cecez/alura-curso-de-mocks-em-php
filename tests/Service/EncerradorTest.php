43a5cd2aaa85f52beefc60b8d1eab6c1743ef491
Cezar Rosa <cecez86@gmail.com>
2020-11-07 14:03:27 -0300

    * injecao de dependencia (conexão PDO) na classe real LeilaoDao * informando mock chamadas de método esperadas, e seus parâmetros

--- tests/Service/EncerradorTest.php
+++ tests/Service/EncerradorTest.php
@@ -16,9 +16,14 @@ class EncerradorTest extends TestCase
         $ipad   = new LeilaoModel('iPad 2020', new \DateTimeImmutable('8 days ago'));
         $iphone = new LeilaoModel('iPhone 2020', new \DateTimeImmutable('10 days ago'));
 
-        // cria e configura mock
+        // cria mock
         $leilaoDao = $this->createMock(LeilaoDao::class);
+        // instrui mock
         $leilaoDao->method('recuperarNaoFinalizados')->willReturn([$ipad, $iphone]);
+        // informa que espera-se duas chamadas ao método atualiza
+        $leilaoDao->expects($this->exactly(2))
+                  ->method('atualiza')
+                  ->withConsecutive([$ipad], [$iphone]);
 
         // chama código a se testar
         $encerrador = new Encerrador($leilaoDao);
